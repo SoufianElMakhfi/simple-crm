01bb5eda36b5eca6a44c367ae5232f8bec68cc5a
SoufianElMakhfi <soufian.elmakhfi@gmail.com>
2024-06-25 13:12:26 +0200

    add user btn

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
+import { MatTooltipModule, TooltipComponent} from '@angular/material/tooltip';
 
 
 @Component({
@@ -12,11 +14,15 @@ import { RouterModule } from '@angular/router';
     MatToolbarModule,
     MatSidenavModule,
     MatIconModule,
-    RouterModule
+    RouterModule,
+    MatButtonModule,
+    MatTooltipModule,
+    TooltipComponent,
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'simple_CRM';
+
 }
