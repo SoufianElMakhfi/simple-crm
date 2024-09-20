0fe73cb9676ca308a3b5a58972b00d6a20eeb112
SoufianElMakhfi <soufian.elmakhfi@gmail.com>
2024-09-20 11:17:00 +0200

    user adding w/ rxjs Observable

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { MatTooltipModule, TooltipComponent } from '@angular/material/tooltip';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 import { User } from '../../models/user.class';
-import {MatCardModule} from '@angular/material/card';
-
-
+import { MatCardModule } from '@angular/material/card';
+import { Firestore, collectionData, collection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user',
@@ -15,26 +16,32 @@ import {MatCardModule} from '@angular/material/card';
   imports: [
     MatButtonModule,
     MatTooltipModule,
-    TooltipComponent,
     MatIconModule,
     MatDialogModule,
     DialogAddUserComponent,
     MatCardModule,
-    
+    CommonModule,
   ],
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss']
 })
 export class UserComponent {
   user = new User();
+  users$: Observable<User[]>; // Typ auf User[] ändern
+  allUsers: User[] = []; // Typisierung für allUsers
 
+  constructor(public dialog: MatDialog, private firestore: Firestore) {
+    const usersCollection = collection(this.firestore, 'users');
+    this.users$ = collectionData(usersCollection) as Observable<User[]>; // Typanpassung hier
 
-  constructor(public dialog: MatDialog) { }
-
-
+    // Abonnieren des Observables, um die Werte zu sehen
+    this.users$.subscribe(users => {
+      console.log('Received from DB:', users);
+      this.allUsers = users; // hier users verwenden
+    });
+  }
 
   openDialog(): void {
-    this.dialog.open(DialogAddUserComponent, {
-    });
+    this.dialog.open(DialogAddUserComponent, {});
   }
 }
