a5f30a824e46f88f6936c76325bd21e736aaf229
SoufianElMakhfi <soufian.elmakhfi@gmail.com>
2024-09-23 18:41:36 +0200

    user routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { UserComponent } from './user/user.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
 
 const routes: Routes = [
     { path: '', component: DashboardComponent },
     { path: 'dashboard', component: DashboardComponent },
     { path: 'user', component: UserComponent },
-];
+    { path: 'user/:id', component: UserDetailComponent },
+
+
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
