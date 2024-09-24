8177ec64aa15f645733e61599e1daa1410c8bc1b
SoufianElMakhfi <soufian.elmakhfi@gmail.com>
2024-09-24 14:54:40 +0200

    user avatar & user details

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-user-detail',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatDialogModule
+  ],
   templateUrl: './user-detail.component.html',
   styleUrl: './user-detail.component.scss'
 })
